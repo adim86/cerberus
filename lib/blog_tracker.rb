8b0ba05d43871f460c6ac60ec66b90c969b92975
Adim Ofunne <aofunne@gmail.com>
2015-02-01 10:20:09 -0700

    changed created at

--- lib/blog_tracker.rb
+++ lib/blog_tracker.rb
@@ -13,8 +13,8 @@ module BlogTracker
           #Feed comeback with newest on top we like it bac
           feed_entries.reverse!
 
-          last_story = Story.where(:job_url => job).order("created_at").last
-          puts last_story
+          last_story = Story.where(:job_url => job).order(created_at: :asc).last
+          puts last_story.inspect
           if (last_story.blank?)
             add_new_stories(feed_entries, job)
           else
